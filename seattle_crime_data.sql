0813df096a3719ece431a02ad4a4f93ad9a10a13
Khang Nguyen <131831732+Khangnguyen04@users.noreply.github.com>
2023-08-31 00:23:24 -0400

    Update seattle_crime_data.sql

--- seattle_crime_data.sql
+++ seattle_crime_data.sql
@@ -383,7 +383,7 @@ order by
 	4. Queen Anne, 3473 crimes/year, 36000 people
 	5. Slu/Cascade, 3021 crimes/year, 29376 people
 		
-    We can conclude that population density has a direct correlation with higher crime rates in the query,
+    We can conclude that population density has a correlation with higher crime rates in the query,
     we will analyze further in Tableau.
 */
 
